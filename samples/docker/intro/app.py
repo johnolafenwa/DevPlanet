86f8462de877b9a3741508370eae78474add8d72
John Olafenwa <johnolafenwa@gmail.com>
2019-09-03 11:59:43 +0000

    endgamereview example

--- samples/docker/intro/app.py
+++ samples/docker/intro/app.py
@@ -5,7 +5,8 @@ app = Flask(__name__)
 @app.route("/")
 def index():
 
-    return "<h1>Hello From Flask</h1>"
+    return """<h1>Hello From Flask</h1>
+    <h2> Version Three of Dockerland </h2>"""
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0",port=5000)
